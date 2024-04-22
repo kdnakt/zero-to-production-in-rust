c3856794ba96ce10f919bdf8cf23645b18ce23a7
kdnakt <a.kid.1985@gmail.com>
2024-04-23 01:05:53 +0900

    use random port on test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use actix_web::{
     web, App, HttpRequest, HttpResponse, HttpServer, Responder,
     dev::Server,
 };
+use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -12,14 +13,14 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
