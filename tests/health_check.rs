e4b8da34500bc7fcc2b50224c32129e8c4f2f54e
kdnakt <a.kid.1985@gmail.com>
2024-04-28 00:36:45 +0900

    fix query macro

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -46,7 +46,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&mut conn)
         .await
         .expect("Failed to fetch saved subscription");
