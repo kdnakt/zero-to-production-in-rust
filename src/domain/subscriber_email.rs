ac499f1f455c378b7bd3e516a87d065dfd5982e2
kdnakt <a.kid.1985@gmail.com>
2024-05-16 09:20:11 +0900

    validate_email()

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,10 +1,15 @@
+use validator::ValidateEmail;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
-        // TODO: add validation
-        Ok(Self(s))
+        if s.validate_email() {
+            Ok(Self(s))
+        } else {
+            Err(format!("{s} is not a valid subscriber email."))
+        }
     }
 }
 
