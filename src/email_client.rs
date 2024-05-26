53a96bd2de935a03403455d72e4dc11e64102930
kdnakt <a.kid.1985@gmail.com>
2024-05-26 20:23:14 +0900

    add constructor

--- src/email_client.rs
+++ src/email_client.rs
@@ -9,6 +9,14 @@ pub struct EmailClient {
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
