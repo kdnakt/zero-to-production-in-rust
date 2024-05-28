90adb79d4e31dd3d5da59bffe366877fa38f72d8
kdnakt <a.kid.1985@gmail.com>
2024-05-28 09:39:52 +0900

    use email client in main

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use std::net::TcpListener;
 
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
-    configuration::get_configuration,
-    startup::run,
-    telemetry::{get_subscriber, init_subscriber},
+    configuration::get_configuration, email_client::EmailClient, startup::run, telemetry::{get_subscriber, init_subscriber}
 };
 
 #[tokio::main]
@@ -16,10 +14,17 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration.email_client.sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email
+    );
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
