94a88531fc65e3cbe46de3c817907025288f7a1d
kdnakt <a.kid.1985@gmail.com>
2024-08-19 02:33:51 +0900

    add AuthError

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -29,6 +29,8 @@ struct ConfirmedSubscriber {
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed.")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -43,6 +45,7 @@ impl ResponseError for PublishError {
     fn status_code(&self) -> actix_web::http::StatusCode {
         match self {
             PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::AuthError(_) => StatusCode::UNAUTHORIZED,
         }
     }
 }
@@ -53,7 +56,7 @@ pub async fn publish_newsletter(
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers());
+    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
