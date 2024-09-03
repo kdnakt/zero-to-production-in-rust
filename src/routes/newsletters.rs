483303ee314d5ca55d4aa3b361ce3d4181ff7c56
kdnakt <a.kid.1985@gmail.com>
2024-09-03 10:14:36 +0900

    use password verifier

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -6,11 +6,10 @@ use actix_web::{
     web, HttpRequest, HttpResponse, ResponseError,
 };
 use anyhow::Context;
-use argon2::{Argon2, Params};
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use base64::prelude::*;
 use secrecy::ExposeSecret;
 use secrecy::Secret;
-use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient};
@@ -147,16 +146,9 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let hasher = Argon2::new(
-        argon2::Algorithm::Argon2id,
-        argon2::Version::V0x13,
-        Params::new(15000, 2, 1, None)
-            .context("Failed to build Argon2 parameters")
-            .map_err(PublishError::UnexpectedError)?,
-    );
     let row: Option<_> = sqlx::query!(
         r#"
-        SELECT user_id, password_hash, salt
+        SELECT user_id, password_hash
         FROM users
         WHERE username = $1
         "#,
@@ -166,36 +158,25 @@ async fn validate_credentials(
     .await
     .context("Failed to perform a query to retrieve stored credentials.")
     .map_err(PublishError::UnexpectedError)?;
-    let (expected_password_hash, user_id, salt) = match row {
-        Some(row) => (row.password_hash, row.user_id, row.salt),
+    let (expected_password_hash, user_id) = match row {
+        Some(row) => (row.password_hash, row.user_id),
         None => {
             return Err(PublishError::AuthError(anyhow::anyhow!(
                 "Unknown username."
             )))
         }
     };
-    let mut password_hash = Vec::new();
-    hasher
-        .hash_password_into(
-            credentials.password.expose_secret().as_bytes(),
-            salt.as_bytes(),
-            &mut password_hash,
-        )
-        .context("Failed to hash password")
+    let expected_password_hash = PasswordHash::new(&expected_password_hash)
+        .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
-    let password_hash = password_hash
-        .iter()
-        .map(|byte| format!("{:x}", byte))
-        .collect::<Vec<String>>()
-        .join("");
-
-    if password_hash != expected_password_hash {
-        Err(PublishError::AuthError(anyhow::anyhow!(
-            "Invalid password."
-        )))
-    } else {
-        Ok(user_id)
-    }
+    Argon2::default()
+        .verify_password(
+            credentials.password.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password.")
+        .map_err(PublishError::AuthError)?;
+    Ok(user_id)
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
