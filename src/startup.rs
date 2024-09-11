84d3e728c4d68b1f2be0d2fbb4492bd95cfe84bd
kdnakt <a.kid.1985@gmail.com>
2024-09-12 08:39:47 +0900

    add home module

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use tracing_actix_web::TracingLogger;
 use crate::{
     configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
-    routes::{confirm, health_check, publish_newsletter, subscribe},
+    routes::{confirm, health_check, home, publish_newsletter, subscribe},
 };
 
 pub struct Application {
@@ -35,6 +35,7 @@ pub fn run(
             .route("subscriptions", web::post().to(subscribe))
             .route("subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/", web::get().to(home))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
