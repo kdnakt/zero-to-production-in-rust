82f16cebd9e302e2f491efdc113713d41098797e
kdnakt <a.kid.1985@gmail.com>
2024-09-26 22:26:16 -0700

    error query parameter

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,7 +1,13 @@
-use actix_web::{http::header::ContentType, HttpResponse};
+use actix_web::{http::header::ContentType, web, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let _error = query.0.error;
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(include_str!("login.html"))
+}
+
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
 }
