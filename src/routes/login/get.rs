7bd6f3d7283dc22ce01baa74aa5c7df1aecb46d9
kdnakt <a.kid.1985@gmail.com>
2024-10-05 13:45:52 +0900

    verify query params

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,4 +1,6 @@
 use actix_web::{http::header::ContentType, web, HttpResponse};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
 
 use crate::startup::HmacSecret;
 
@@ -50,4 +52,18 @@ pub async fn login_form(
 pub struct QueryParams {
     error: String,
     tag: String,
+}
+
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
 }
