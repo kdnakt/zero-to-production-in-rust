79c054139fff5dc797dad19c8b4d91bbf211e843
kdnakt <a.kid.1985@gmail.com>
2024-10-17 01:42:33 +0900

    remove unused code

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,4 @@
 use actix_web::{cookie::Cookie, http::header::ContentType, HttpRequest, HttpResponse};
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
-
-use crate::startup::HmacSecret;
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
     let error_html = match request.cookie("_flash") {
@@ -49,24 +45,4 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         .add_removal_cookie(&Cookie::new("_flash", ""))
         .unwrap();
     response
-}
-
-#[derive(serde::Deserialize)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
-
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
-
-        let mut mac =
-            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-
-        Ok(self.error)
-    }
 }
