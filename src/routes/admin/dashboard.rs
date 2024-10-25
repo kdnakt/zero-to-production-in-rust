45ea9fba752e2c5901dff3832757175b839adc4f
kdnakt <a.kid.1985@gmail.com>
2024-10-25 23:35:49 +0900

    read session data

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,24 @@
+use actix_session::Session;
 use actix_web::HttpResponse;
+use uuid::Uuid;
 
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+fn e500<T>(e: T) -> actix_web::Error
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static
+{
+    actix_web::error::ErrorInternalServerError(e)
+}
+
+pub async fn admin_dashboard(
+    session: Session
+) -> Result<HttpResponse, actix_web::Error> {
+    let _username = if let Some(user_id) = session
+        .get::<Uuid>("user_id")
+        .map_err(e500)?
+    {
+        todo!()
+    } else {
+        todo!()
+    };
+    Ok(HttpResponse::Ok().finish())
 }
