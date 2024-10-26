e0f18be7cf9e82ebe4463f445cd22ecd40d82e4f
kdnakt <a.kid.1985@gmail.com>
2024-10-26 23:51:17 +0900

    read username from database

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,24 +1,39 @@
 use actix_session::Session;
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use anyhow::Context;
+use sqlx::PgPool;
 use uuid::Uuid;
 
 fn e500<T>(e: T) -> actix_web::Error
 where
-    T: std::fmt::Debug + std::fmt::Display + 'static
+    T: std::fmt::Debug + std::fmt::Display + 'static,
 {
     actix_web::error::ErrorInternalServerError(e)
 }
 
 pub async fn admin_dashboard(
-    session: Session
+    session: Session,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let _username = if let Some(user_id) = session
-        .get::<Uuid>("user_id")
-        .map_err(e500)?
-    {
-        todo!()
+    let _username = if let Some(user_id) = session.get::<Uuid>("user_id").map_err(e500)? {
+        get_username(user_id, &pool).await.map_err(e500)?
     } else {
         todo!()
     };
     Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get username", skip(pool))]
+async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT username FROM users
+        WHERE user_id = $1
+    "#,
+        user_id
+    )
+    .fetch_one(pool)
+    .await
+    .context("Failed to perform a query to retrieve a username")?;
+    Ok(row.username)
 }
