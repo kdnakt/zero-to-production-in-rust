86c65490ac835a2a69fee70c82ff1a805e6e2442
kdnakt <a.kid.1985@gmail.com>
2024-10-30 10:05:10 +0900

    implement FromRequest

--- src/session_state.rs
+++ src/session_state.rs
@@ -1,4 +1,7 @@
-use actix_session::{Session, SessionGetError, SessionInsertError};
+use std::future::{ready, Ready};
+
+use actix_session::{Session, SessionExt, SessionGetError, SessionInsertError};
+use actix_web::FromRequest;
 use uuid::Uuid;
 
 pub struct TypedSession(Session);
@@ -17,5 +20,17 @@ impl TypedSession {
     pub fn get_user_id(&self) -> Result<Option<Uuid>, SessionGetError> {
         self.0.get(Self::USER_ID_KEY)
     }
+}
 
+impl FromRequest for TypedSession {
+    type Error = <Session as FromRequest>::Error;
+
+    type Future = Ready<Result<TypedSession, Self::Error>>;
+
+    fn from_request(
+        req: &actix_web::HttpRequest,
+        _payload: &mut actix_web::dev::Payload,
+    ) -> Self::Future {
+        ready(Ok(TypedSession(req.get_session())))
+    }
 }
