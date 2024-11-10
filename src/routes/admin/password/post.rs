da7467923a426f0fbef42f477191c8a3b972455a
kdnakt <a.kid.1985@gmail.com>
2024-11-11 02:15:58 +0900

    show flash messages on change password form

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 
 use crate::utils::see_other;
@@ -12,6 +13,10 @@ pub struct FormData {
 
 pub async fn change_password(form: web::Form<FormData>) -> Result<HttpResponse, actix_web::Error> {
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        FlashMessage::error(
+            "You entered two different new passwords - the field values must match.",
+        )
+        .send();
         return Ok(see_other("/admin/password"));
     }
     todo!()
