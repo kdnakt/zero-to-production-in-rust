491837b659d765766cc40559595e5a104a9dbd49
kdnakt <a.kid.1985@gmail.com>
2024-11-12 07:49:17 +0900

    check session

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,8 +1,14 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
-use crate::utils::see_other;
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::admin::dashboard::get_username,
+    session_state::TypedSession,
+    utils::{e500, see_other},
+};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,7 +17,17 @@ pub struct FormData {
     new_password_check: Secret<String>,
 }
 
-pub async fn change_password(form: web::Form<FormData>) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password(
+    form: web::Form<FormData>,
+    session: web::Data<TypedSession>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let user_id = session.get_user_id().map_err(e500)?;
+    if user_id.is_none() {
+        return Ok(see_other("/login"));
+    }
+    let user_id = user_id.unwrap();
+
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "You entered two different new passwords - the field values must match.",
@@ -19,5 +35,22 @@ pub async fn change_password(form: web::Form<FormData>) -> Result<HttpResponse, 
         .send();
         return Ok(see_other("/admin/password"));
     }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+        };
+    }
+
     todo!()
 }
