78d94ecdde7e12fd883127693dbea1224ec376b2
kdnakt <a.kid.1985@gmail.com>
2025-02-28 01:37:02 +0900

    make progress concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use zero2prod::{
     configuration::get_configuration,
+    issue_delivery_worker::run_worker_until_stopped,
     startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -10,7 +11,15 @@ async fn main() -> anyhow::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = Application::build(configuration.clone()).await?;
+    let app_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
+
+    // make progress concurrently
+    tokio::select! {
+        _ = app_task => {},
+        _ = worker_task => {},
+    };
+
     Ok(())
 }
